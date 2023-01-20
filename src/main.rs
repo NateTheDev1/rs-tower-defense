9f93c44159bbc76405cd2e0d0ad8b0633ea6a40e
Nathaniel Richards <nthnlrichards@gmail.com>
2023-01-20 16:43:47 -0500

    Selectable towers

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,63 @@ mod bullet;
 mod target;
 mod tower;
 
+use bevy_mod_picking::{
+    DefaultPickingPlugins, Highlighting, PickableBundle, PickingCameraBundle, Selection,
+};
 pub use bullet::*;
 pub use target::*;
 pub use tower::*;
 
 use bevy::{
+    ecs::query::QuerySingleError,
+    pbr::NotShadowCaster,
     prelude::{
-        default, shape, App, AssetServer, Assets, Camera3d, Camera3dBundle, ClearColor, Color,
-        Commands, Handle, Input, KeyCode, Mesh, Name, PbrBundle, PluginGroup, PointLight,
-        PointLightBundle, Query, Res, ResMut, Resource, StandardMaterial, StartupStage, Transform,
-        Vec3, With,
+        default, shape, App, AssetServer, Assets, BuildChildren, ButtonBundle, Camera3d,
+        Camera3dBundle, Changed, ClearColor, Color, Commands, Component, DespawnRecursiveExt,
+        Entity, Handle, Input, KeyCode, Mesh, Name, NodeBundle, PbrBundle, PluginGroup, PointLight,
+        PointLightBundle, Query, Res, ResMut, Resource, SpatialBundle, StandardMaterial,
+        StartupStage, Transform, Vec3, With,
     },
     scene::{Scene, SceneBundle},
-    time::{Time, Timer},
+    time::{Time, Timer, TimerMode},
+    ui::{AlignSelf, Interaction, JustifyContent, Size, Style, UiRect, Val},
     window::{WindowDescriptor, WindowPlugin},
     DefaultPlugins,
 };
 
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WIDTH,
+                height: HEIGHT,
+                title: "Tower Defense".to_string(),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugin(WorldInspectorPlugin)
+        .add_plugins(DefaultPickingPlugins)
+        .add_plugin(TowerPlugin)
+        .add_plugin(TargetPlugin)
+        .add_plugin(BulletPlugin)
+        .add_startup_system_to_stage(StartupStage::PreStartup, asset_loading)
+        .add_startup_system(spawn_basic_scene)
+        .add_startup_system(spawn_lighting)
+        .add_startup_system(spawn_camera)
+        .add_system(create_ui_on_selection)
+        .add_system(camera_controls)
+        .add_system(what_is_selected)
+        .add_system(tower_button_clicked)
+        .run();
+}
 
 pub fn spawn_lighting(mut commands: Commands) {
     commands
@@ -42,10 +81,9 @@ pub fn spawn_basic_scene(
     game_assets: Res<GameAssets>,
 ) {
     commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.4 })),
-            material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
-            transform: Transform::from_xyz(-2.0, 0.2, 1.5),
+        .spawn(SceneBundle {
+            scene: game_assets.target_scene.clone(),
+            transform: Transform::from_xyz(-2.0, 0.4, 2.5),
             ..Default::default()
         })
         .insert(Target { speed: 0.3 })
@@ -53,10 +91,9 @@ pub fn spawn_basic_scene(
         .insert(Name::new("Target"));
 
     commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.4 })),
-            material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
-            transform: Transform::from_xyz(-4.0, 0.2, 1.5),
+        .spawn(SceneBundle {
+            scene: game_assets.target_scene.clone(),
+            transform: Transform::from_xyz(-4.0, 0.4, 2.5),
             ..Default::default()
         })
         .insert(Target { speed: 0.3 })
@@ -66,26 +103,62 @@ pub fn spawn_basic_scene(
     commands
         .spawn(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Plane { size: 20.0 })),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            material: materials.add(Color::rgb(0.3, 0.4, 0.3).into()),
             ..Default::default()
         })
         .insert(Health { value: 3 })
         .insert(Name::new("Ground"));
 
-    commands
-        .spawn(SceneBundle {
-            scene: game_assets.tower_base_scene.clone(),
-            ..default()
-        })
-        .insert(Name::new("Tower"));
+    let default_collider_color = materials.add(Color::rgba(0.3, 0.5, 0.3, 0.3).into());
+
+    let selected_collider_color = materials.add(Color::rgba(0.3, 0.9, 0.3, 0.9).into());
+
+    for i in 0..5 {
+        commands
+            .spawn(SpatialBundle::from_transform(Transform::from_xyz(
+                0.0 + 0.5 + i as f32,
+                0.8,
+                0.0,
+            )))
+            .insert(Name::new("Tower_Base"))
+            .insert(meshes.add(shape::Capsule::default().into()))
+            .insert(Highlighting {
+                initial: default_collider_color.clone(),
+                hovered: Some(selected_collider_color.clone()),
+                pressed: Some(selected_collider_color.clone()),
+                selected: Some(selected_collider_color.clone()),
+            })
+            .insert(default_collider_color.clone())
+            .insert(NotShadowCaster)
+            .insert(PickableBundle::default())
+            .with_children(|commands| {
+                commands.spawn(SceneBundle {
+                    scene: game_assets.tower_base_scene.clone(),
+                    transform: Transform::from_xyz(0.0, -0.8, 0.0),
+                    ..Default::default()
+                });
+            });
+    }
+}
+
+fn what_is_selected(selection: Query<(&Name, &Selection)>) {
+    for (name, selection) in &selection {
+        if selection.selected() {
+            println!("Selected: {}", name);
+        }
+    }
 }
 
 #[derive(Resource)]
 pub struct GameAssets {
-    pub tower_base_scene: Handle<Scene>,
-    pub tomato_tower_scene: Handle<Scene>,
-    pub tomato_scene: Handle<Scene>,
-    pub target_scene: Handle<Scene>,
+    tower_base_scene: Handle<Scene>,
+    tomato_tower_scene: Handle<Scene>,
+    tomato_scene: Handle<Scene>,
+    potato_tower_scene: Handle<Scene>,
+    potato_scene: Handle<Scene>,
+    cabbage_tower_scene: Handle<Scene>,
+    cabbage_scene: Handle<Scene>,
+    target_scene: Handle<Scene>,
 }
 
 pub fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
@@ -93,15 +166,21 @@ pub fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
         tower_base_scene: assets.load("TowerBase.glb#Scene0"),
         tomato_tower_scene: assets.load("TomatoTower.glb#Scene0"),
         tomato_scene: assets.load("Tomato.glb#Scene0"),
+        potato_tower_scene: assets.load("PotatoTower.glb#Scene0"),
+        potato_scene: assets.load("Potato.glb#Scene0"),
+        cabbage_tower_scene: assets.load("CabbageTower.glb#Scene0"),
+        cabbage_scene: assets.load("Cabbage.glb#Scene0"),
         target_scene: assets.load("Target.glb#Scene0"),
     });
 }
 
 pub fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert(PickingCameraBundle::default());
 }
 
 pub fn camera_controls(
@@ -148,30 +227,145 @@ pub fn camera_controls(
     }
 }
 
-pub const HEIGHT: f32 = 720.0;
-pub const WIDTH: f32 = 1280.0;
+fn tower_button_clicked(
+    interaction: Query<(&Interaction, &TowerType), Changed<Interaction>>,
+    mut commands: Commands,
+    selection: Query<(Entity, &Selection, &Transform)>,
+    assets: Res<GameAssets>,
+) {
+    for (interaction, tower_type) in &interaction {
+        if matches!(interaction, Interaction::Clicked) {
+            for (entity, selection, transform) in &selection {
+                if selection.selected() {
+                    commands.entity(entity).despawn_recursive();
 
-fn main() {
-    App::new()
-        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                width: WIDTH,
-                height: HEIGHT,
-                title: "Tower Defense".to_string(),
-                resizable: false,
+                    spawn_tower(&mut commands, &assets, transform.translation, *tower_type);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct TowerUIRoot;
+
+#[derive(Component, Clone, Copy, Debug)]
+pub enum TowerType {
+    Tomato,
+    Potato,
+    Cabbage,
+}
+
+impl TowerType {
+    pub fn get_tower(&self, assets: &GameAssets) -> (Handle<Scene>, Tower) {
+        match self {
+            TowerType::Tomato => (
+                assets.tomato_tower_scene.clone(),
+                Tower {
+                    shooting_timer: Timer::from_seconds(0.5, TimerMode::Repeating),
+                    bullet_offset: Vec3::new(0.0, 0.6, 0.0),
+                },
+            ),
+            TowerType::Potato => (
+                assets.potato_tower_scene.clone(),
+                Tower {
+                    shooting_timer: Timer::from_seconds(0.1, TimerMode::Repeating),
+                    bullet_offset: Vec3::new(0.0, 0.6, 0.0),
+                },
+            ),
+            TowerType::Cabbage => (
+                assets.cabbage_tower_scene.clone(),
+                Tower {
+                    shooting_timer: Timer::from_seconds(0.8, TimerMode::Repeating),
+                    bullet_offset: Vec3::new(0.0, 0.6, 0.0),
+                },
+            ),
+        }
+    }
+
+    pub fn get_bullet(&self, direction: Vec3, assets: &GameAssets) -> (Handle<Scene>, Bullet) {
+        match self {
+            TowerType::Tomato => (
+                assets.tomato_scene.clone(),
+                Bullet {
+                    direction,
+                    speed: 3.5,
+                },
+            ),
+            TowerType::Potato => (
+                assets.potato_scene.clone(),
+                Bullet {
+                    direction,
+                    speed: 6.5,
+                },
+            ),
+            TowerType::Cabbage => (
+                assets.cabbage_scene.clone(),
+                Bullet {
+                    direction,
+                    speed: 1.5,
+                },
+            ),
+        }
+    }
+}
+
+fn create_ui_on_selection(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    selections: Query<&Selection>,
+    root: Query<Entity, With<TowerUIRoot>>,
+) {
+    let at_least_one_selected = selections.iter().any(|selection| selection.selected());
+    match root.get_single() {
+        Ok(root) => {
+            if !at_least_one_selected {
+                commands.entity(root).despawn_recursive();
+            }
+        }
+        //No root exist
+        Err(QuerySingleError::NoEntities(..)) => {
+            if at_least_one_selected {
+                create_ui(&mut commands, &asset_server);
+            }
+        }
+        _ => unreachable!("Too many ui tower roots!"),
+    }
+}
+
+pub fn create_ui(commands: &mut Commands, asset_server: &AssetServer) {
+    let towers = [TowerType::Tomato, TowerType::Potato, TowerType::Cabbage];
+
+    let button_icons = [
+        asset_server.load("tomato_tower.png"),
+        asset_server.load("potato_tower.png"),
+        asset_server.load("cabbage_tower.png"),
+    ];
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::Center,
                 ..default()
             },
             ..default()
-        }))
-        .add_plugin(WorldInspectorPlugin)
-        .add_plugin(TowerPlugin)
-        .add_plugin(TargetPlugin)
-        .add_plugin(BulletPlugin)
-        .add_startup_system_to_stage(StartupStage::PreStartup, asset_loading)
-        .add_startup_system(spawn_basic_scene)
-        .add_startup_system(spawn_lighting)
-        .add_startup_system(spawn_camera)
-        .add_system(camera_controls)
-        .run();
+        })
+        .insert(TowerUIRoot)
+        .with_children(|commands| {
+            for i in 0..3 {
+                commands
+                    .spawn(ButtonBundle {
+                        style: Style {
+                            size: Size::new(Val::Percent(15.0 * 9.0 / 16.0), Val::Percent(15.0)),
+                            align_self: AlignSelf::FlexEnd,
+                            margin: UiRect::all(Val::Percent(2.0)),
+                            ..default()
+                        },
+                        image: button_icons[i].clone().into(),
+                        ..default()
+                    })
+                    .insert(towers[i]);
+            }
+        });
 }
--- src/tower.rs
+++ src/tower.rs
@@ -1,15 +1,16 @@
 use bevy::{
     prelude::{
-        App, BuildChildren, Commands, Component, Entity, GlobalTransform, Name, Plugin, Query, Res,
-        Transform, Vec3, With,
+        App, BuildChildren, Commands, Component, DespawnRecursiveExt, Entity, GlobalTransform,
+        Input, KeyCode, Name, Plugin, Query, Res, SpatialBundle, Transform, Vec3, With,
     },
     reflect::Reflect,
     scene::SceneBundle,
     time::{Time, Timer, TimerMode},
     utils::FloatOrd,
 };
+use bevy_mod_picking::Selection;
 
-use crate::{Bullet, GameAssets, Lifetime, Target};
+use crate::{Bullet, GameAssets, Lifetime, Target, TowerType};
 
 #[derive(Component, Reflect, Default)]
 pub struct Tower {
@@ -21,18 +22,20 @@ pub struct TowerPlugin;
 
 impl Plugin for TowerPlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<Tower>().add_system(tower_shooting);
+        app.register_type::<Tower>()
+            .add_system(tower_shooting)
+            .add_system(build_tower);
     }
 }
 
 fn tower_shooting(
     mut commands: Commands,
-    mut towers: Query<(Entity, &mut Tower, &GlobalTransform)>,
+    mut towers: Query<(Entity, &mut Tower, &TowerType, &GlobalTransform)>,
     targets: Query<&GlobalTransform, With<Target>>,
     game_assets: Res<GameAssets>,
     time: Res<Time>,
 ) {
-    for (tower_ent, mut tower, transform) in &mut towers {
+    for (tower_ent, mut tower, tower_type, transform) in &mut towers {
         tower.shooting_timer.tick(time.delta());
         if tower.shooting_timer.just_finished() {
             let bullet_spawn = transform.translation() + tower.bullet_offset;
@@ -45,22 +48,67 @@ fn tower_shooting(
                 .map(|closest_target| closest_target.translation() - bullet_spawn);
 
             if let Some(direction) = direction {
+                let (model, bullet) = tower_type.get_bullet(direction, &game_assets);
+
                 commands.entity(tower_ent).with_children(|commands| {
                     commands
                         .spawn(SceneBundle {
-                            scene: game_assets.tomato_scene.clone(),
+                            scene: model,
                             transform: Transform::from_translation(tower.bullet_offset),
                             ..Default::default()
                         })
                         .insert(Lifetime {
                             timer: Timer::from_seconds(1000.5, TimerMode::Once),
                         })
-                        .insert(Bullet {
-                            direction,
-                            speed: 2.5,
-                        })
+                        .insert(bullet)
                         .insert(Name::new("Bullet"));
                 });
+            }
+        }
+    }
+}
+
+pub fn spawn_tower(
+    commands: &mut Commands,
+    game_assets: &GameAssets,
+    position: Vec3,
+    tower_type: TowerType,
+) -> Entity {
+    let (tower_scene, tower) = tower_type.get_tower(game_assets);
+
+    commands
+        .spawn(SpatialBundle::from_transform(Transform::from_translation(
+            position,
+        )))
+        .insert(Name::new(format!("{:?}_Tower", tower_type)))
+        .insert(tower_type)
+        .insert(tower)
+        .with_children(|commands| {
+            commands.spawn(SceneBundle {
+                scene: tower_scene,
+                transform: Transform::from_xyz(0.0, -0.8, 0.0),
+                ..Default::default()
+            });
+        })
+        .id()
+}
+
+pub fn build_tower(
+    mut commands: Commands,
+    selection: Query<(Entity, &Selection, &Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    game_assets: Res<GameAssets>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        for (entity, selection, transform) in &selection {
+            if selection.selected() {
+                commands.entity(entity).despawn_recursive();
+                spawn_tower(
+                    &mut commands,
+                    &game_assets,
+                    transform.translation,
+                    TowerType::Tomato,
+                );
             }
         }
     }
